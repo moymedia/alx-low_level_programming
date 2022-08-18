d0d4efba87594474741914dcda813ee72f368b7d
moymedia <moymedia296@gmail.com>
2022-08-18 12:57:58 -0700

    hot fix task 5,6

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * clear_bit - clear the bit at a given index
- * @n: number to check
- * @index: index to clear
- * Return: bit value or -1;
+ * clear_bit - Sets the value of a bit at a given index to 0.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - 1.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i = 1, len = 0;
-	
-	if (!n)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	while (len++ < index)
-		i = i << 1;
-	if (*n & i)
-		*n ^= i;
+
+	*n &= ~(1 << index);
+
 	return (1);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * flip_bits - returns no of bits to flip for two numbers to match
- * @n: first number
- * @m: second number
- * Return: number of bits to flip;
+ * flip_bits - returns hamming distance of two bit words
+ * which is the number of bits you would need to flip to get
+ * from one number to another
+ * @n: the first bit word
+ * @m: the second bit word
+ *
+ * Return: the hamming distance
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int tmp, i = 1, count = 0;
-	
-	/* find the different bits */
-	tmp = n ^ m;
-	/* count ones in tmp */
-	while (tmp >= i)
-	{
-		if (i & tmp)
-			count++;
-		i = i << 1;
-	}
-	return (count);
+	int shift = (sizeof(n) * 8);
+	int dist = 0;
+
+	while (shift--)
+		dist += (n >> shift & 1) != (m >> shift & 1);
+	return (dist);
 }
